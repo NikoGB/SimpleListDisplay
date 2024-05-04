b32322357d3bcb889f102816bd6502857ec617f9
NikoGB <114112855+NikoGB@users.noreply.github.com>
2024-05-04 15:55:22 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
-    rewrites(){
-        return [
-            {
-              source: '/backend/:path*',
-              destination: 'http://localhost:3001/:path*'
-            }
-        ]
-    }
 }
 module.exports = nextConfig
