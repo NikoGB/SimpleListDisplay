baa9566484bba59f6e1cf945a104bddec6a4b2fb
NikoGB <114112855+NikoGB@users.noreply.github.com>
2025-04-28 21:19:03 -0300

    Update DisplayListItem.jsx

--- src/components/DisplayListItem.jsx
+++ src/components/DisplayListItem.jsx
@@ -6,11 +6,11 @@ const DisplayListItem = ({ number, name }) => {
         <div className="flex items-center justify-between p-2 w-[90vw]">
             
             <div className='rounded-md bg-white w-[20%] text-center py-3 mr-5 shadow-lg' >
-                <span className="text-black font-bold text-[5vh]">#{number}</span> 
+                <span className="text-black font-bold text-[5vh]">{number}</span> 
             </div>
 
             <div className='rounded-md bg-white w-[80%]  text-center py-3 shadow-lg' >
-                <span className="text-black font-thin text-[5vh]">@{name}</span>
+                <span className="text-black font-thin text-[5vh]">{name}</span>
             </div>
         </div>
     );
